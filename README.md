d3b8f7dae35c3c847f512d8998efd2319d13e19c
jvog <62656589+jensvog@users.noreply.github.com>
2020-04-21 19:09:23 +0200

    Creates serverless plugin creating postman env
    Plugin for serverless application. Provides a postman environment file
    with the http api endpoint information. This information can be used by
    testing applications.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # serverless-postman-env-plugin
-"Serverless plugin for creating a postman environment file from http endpoints
+
+Serverless plugin for creating a postman environment file from http endpoints. After
+deploying this plugin can be used to create a postman environment file. The file contains
+information about the http api endpoint. This environment file can be used to start
+postman tests against the deployed api.
+
+## Installation
+
+Plugin can be installed with:
+
+```npm install serverless-postman-env-plugin```
+
+## Usage
+
+The plugin can be called with:
+
+```serverless postmanenv```
+
+After finish a new file called `postman_environment.json` is in the workspace.
+
+## License
+
+See `LICENSE` file for license information.
---
+++ index.js
@@ -0,0 +1,55 @@
+'use strict';
+
+var fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+
+class ServerlessPlugin {
+  constructor(serverless, options) {
+    this.serverless = serverless;
+    this.options = options;
+    this.provider = this.serverless.getProvider('aws');
+
+    this.commands = {
+      postmanenv: {
+        usage: 'Creates a postman environment file from http endpoints',
+        lifecycleEvents: ['create'],
+        options: {
+        },
+      },
+    };
+
+    this.hooks = {
+      'postmanenv:create': this.createPostmanEnv.bind(this)
+    };
+  }
+
+  createPostmanEnv() {
+    this.serverless.cli.log('Create Postman environment file');
+
+    const stackName = this.provider.naming.getStackName(this.options.stage);
+
+    this.provider.request('CloudFormation', 'describeStacks', { stackName }, this.options.stage, this.options.region)
+    .then(response => {
+      const endpoint = response.Stacks[0].Outputs
+      .find(service => service.OutputKey === 'ServiceEndpoint')
+      .OutputValue;
+
+      var postmanenv = {
+        id: uuidv4(),
+        name: stackName,
+        values: [
+          {
+            key: 'apiurl',
+            value: endpoint,
+            enabled: true
+          }
+        ]
+      }
+
+      fs.writeFileSync('./postman_environment.json', JSON.stringify(postmanenv, null, 2));
+      this.serverless.cli.log('Created postman_environment.json');
+    });
+  }
+}
+
+module.exports = ServerlessPlugin;
